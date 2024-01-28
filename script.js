5163fb89290c70d4dea0f366b99fa5dbbcc47a9a
Yordani <yalvarezb93@gmail.com>
2024-01-28 16:21:20 +0300

    Arrows animation added

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 const buttonFeatures = document.getElementById("Features");
 const features = document.getElementById("FeaturesContent");
-
 const buttonCompany = document.getElementById("Company");
 const company = document.getElementById("CompanyContent");
-
-
+const featuresArrow = document.getElementById("FeaturesArrow");
+const companyArrow = document.getElementById("CompanyArrow");
 
 buttonFeatures.addEventListener("click", function(){
     if(features.style.display === "none"){
@@ -15,6 +14,7 @@ buttonFeatures.addEventListener("click", function(){
     }
 })
 
+
 buttonCompany.addEventListener("click", function(){
     if(company.style.display === "none"){
         company.style.display = "flex";
@@ -23,4 +23,22 @@ buttonCompany.addEventListener("click", function(){
         company.style.display = "none";
       
     }
+})
+
+
+buttonFeatures.addEventListener("click", function(){
+    if(featuresArrow.style.transform === "rotate(0deg)"){
+        featuresArrow.style.transform = "rotate(180deg)";
+    }else{
+        featuresArrow.style.transform = "rotate(0deg)";
+    }    
+})
+
+
+buttonCompany.addEventListener("click", function(){
+    if(companyArrow.style.transform === "rotate(0deg)"){
+        companyArrow.style.transform = "rotate(180deg)";
+    }else{
+        companyArrow.style.transform = "rotate(0deg)";
+    }    
 })
